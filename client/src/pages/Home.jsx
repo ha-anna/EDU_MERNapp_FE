65be21d6fa404abdfe7b2ded1ee2e4a028e9cc42
Ha Anna <its.haanna@gmail.com>
2022-10-01 04:05:01 +0900

    Upload

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import Axios from "axios";
+import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+
+function Home() {
+  const { workouts, dispatch } = useWorkoutsContext();
+
+  useEffect(() => {
+    Axios.get("/api/workouts").then((response) => {
+      dispatch({ type: "SET_WORKOUTS", payload: response.data });
+    });
+  }, [dispatch]);
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts &&
+          workouts.map((workout) => (
+            <WorkoutDetails key={workout._id} workout={workout} />
+          ))}
+      </div>
+      <WorkoutForm />
+    </div>
+  );
+}
+
+export default Home;
---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+const express = require("express")
+const mongoose = require('mongoose')
+const app = express()
+const workoutsRoutes = require('./routes/workouts')
+
+app.use(express.json())
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+app.use('/api/workouts', workoutsRoutes)
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+      app.listen(process.env.PORT, () => {
+      console.log('connected to db and listening on port 4000')
+        })
+    })
+    .catch((error) => {
+  console.log(error)
+})
